f7655c53c2b0bdcacd5f3528d79f56dfbeb0cd86
Vlad <cv33fdg@gmail.com>
2024-11-13 08:54:31 +0200

    middle commit 2

--- src/app/[locale]/channel/detailvideo/page.tsx
+++ src/app/[locale]/channel/detailvideo/page.tsx
@@ -3,17 +3,6 @@ import React, { useEffect, useState, useRef } from 'react';
 import HeaderHome from "@/components/pages/home/header/header";
 import AsideHome from "@/components/pages/home/aside/aside";
 import initTranslations from "@/app/i18n";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
-import { Upload, X, Info, Plus, Badge } from "lucide-react"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { useUser } from '@clerk/nextjs';
-import { IUser } from '@/types/user.interface';
-import api from '@/services/axiosApi';
 import VideoUploadInterface from "@/components/pages/channel/videoupload/videoupload"
 
 interface IHomeProps {
