d83415feb9775c2e72340bc8c9ffe9390e062123
Maksym <mtovkay@gmail.com>
2025-01-04 21:45:31 +0100

    base url

--- src/services/axiosApi.ts
+++ src/services/axiosApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-     baseURL: 'http://localhost:5024/api',
-  //  baseURL: 'http://95.217.212.221:5024/api',
+     //baseURL: 'http://localhost:5024/api',
+     baseURL: 'http://95.217.212.221:5024/api',
 });
 
 export default api;
