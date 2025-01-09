1a63d50a23d24489e15868f4675e37b9148fa376
Elena <aleklenoch@gmail.com>
2025-01-09 14:37:23 +0200

    #

--- src/app/[locale]/channel/editing/subtitles/page.tsx
+++ src/app/[locale]/channel/editing/subtitles/page.tsx
@@ -25,7 +25,7 @@ const channelPage = async ({params: {locale}}: ISubtitlesProps) => {
             <div className="flex-auto overflow-hidden pt-20">
 
                 <div className="flex-1 p-6">
-                    <h1 className="text-2xl font-semibold mb-6">Subtitles for videos on the channel</h1>
+                    <h1 className="text-2xl font-semibold mb-6">{t("subtitles:sub")}</h1>
 
                     <div className="flex items-center space-x-9 text-sm">
                         <Link target={'_self'} href={"/channel/editing/subtitles"}
@@ -33,11 +33,11 @@ const channelPage = async ({params: {locale}}: ISubtitlesProps) => {
                               style={{
                                   backgroundColor: 'black', color: 'white', fontWeight: 'bold',
                                   padding: '10px', borderRadius: "8px"
-                              }}>All</Link>
+                              }}>{t("subtitles:all")}</Link>
                         <Link target={'_self'} href={"/channel/editing/subtitles/foulcopy"}
-                              className="text-gray-700 border-b-2 border-gray-900">Foul copy</Link>
+                              className="text-gray-700 border-b-2 border-gray-900">{t("subtitles:foul")}</Link>
                         <Link target={'_self'} href={"/channel/editing/subtitles/published"}
-                              className="text-gray-700 border-b-2 border-gray-900">Published</Link>
+                              className="text-gray-700 border-b-2 border-gray-900">{t("subtitles:pub")}</Link>
                     </div>
 
                     <hr className="my-4 border-gray-300"/>
