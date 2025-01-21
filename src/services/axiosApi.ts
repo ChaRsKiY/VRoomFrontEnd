fe02fb82bcc4744738a8e0abab5cfb560d23e053
Maksym <mtovkay@gmail.com>
2025-01-21 19:51:14 +0100

    ngrok integration

--- src/services/axiosApi.ts
+++ src/services/axiosApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
      //baseURL: 'http://localhost:5024/api',
-     baseURL: "/webapi"
+     baseURL: "https://c7ad-195-230-183-105.ngrok-free.app/api"
 });
 
 export default api;
